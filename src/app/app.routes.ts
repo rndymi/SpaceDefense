45fac810680f344e05956ed06bea5bd8c18031b9
Randy <rndymi@users.noreply.github.com>
2025-11-26 04:02:13 +0100

    Initial Commit. Randy Mendez

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Preferences } from './preferences/preferences';
+import { Play } from './play/play';
+import { Records } from './records/records';
+import { Register } from './register/register';
+import { Login } from './login/login';
+import { Logout } from './logout/logout';
+import { NotFound } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: Home },
+    { path: 'preferences', component: Preferences },
+    { path: 'play', component: Play },
+    { path: 'records', component: Records },
+    { path: 'register', component: Register },
+    { path: 'login', component: Login },
+    { path: 'logout', component: Logout },
+    { path: '**', component: NotFound }
+];
---
+++ src/app/preferences/preferences.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preferences',
+  imports: [],
+  templateUrl: './preferences.html',
+  styleUrl: './preferences.css',
+})
+export class Preferences {
+
+}
