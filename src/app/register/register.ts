78496ee2cfade5e1abca49c8eaa4995ac4ed13c0
Randy <rndymi@users.noreply.github.com>
2025-11-27 05:25:49 +0100

    feat: Register implemented

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+
+  username: string = "";
+  email: string = "";
+  password: string = "";
+  repeatPassword: string = "";
+
+  errorMessage: string = "";
+
+  @ViewChild('snackbar', { static: false }) snackbar!: ElementRef;
+
+  constructor() { }
+
+  createAccount() {
+    this.errorMessage = "";
+
+    if (!this.username || !this.email || !this.password || !this.repeatPassword) {
+      this.errorMessage = 'All fields are required.';
+      return;
+    }
+    if (this.password.length < 6) {
+      this.errorMessage = 'Password must be at least 6 characters.';
+      return;
+    }
+    if (this.password !== this.repeatPassword) {
+      this.errorMessage = 'Passwords do not match.';
+      return;
+    }
+
+    this.showSnackbar();
+
+    this.username = "";
+    this.email = "";
+    this.password = "";
+    this.repeatPassword = "";
+
+  }
+
+  private showSnackbar() {
+    if (!this.snackbar) return;
+
+    const sb = this.snackbar.nativeElement;
+    sb.classList.add('show');
+
+    setTimeout(() => {
+      sb.classList.remove('show');
+    }, 2400);
+  }
 
 }
