f9fd91c4b5801259540dc4e1a22c213ae0717215
Randy <rndymi@users.noreply.github.com>
2025-12-01 03:52:12 +0100

    feat: Update UI based on auth state

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthState } from '../shared/services/auth-state';
+import { CommonModule } from '@angular/common';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {
+export class Navbar implements AfterViewInit {
+
+  constructor(public auth: AuthState) {}
+
+  ngAfterViewInit(): void {
+    const tooltipTriggerList = [].slice.call(
+      document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    );
+    tooltipTriggerList.map((el: any) => new bootstrap.Tooltip(el));
+  }
+
+  logout() {
+    this.auth.logout();
+  }
 
 }
